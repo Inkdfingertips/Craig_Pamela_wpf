465b501df2d10f62c94e4bdc4635673cb435489e
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 03:38:32 -0400

    Wacky Expression
    Forgot all the semicolons, fixed.

--- Craig_Pamela_Wacky_Expression/js/script.js
+++ Craig_Pamela_Wacky_Expression/js/script.js
@@ -1,11 +1,11 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Wacky Expression//
-var age = 27
-var height = 6.0
-var weight = 205
-var birthLocation = "Chicago"
-var towelCount = 9
-var travlerinformation = [age, height, weight,birthLocation, towelCount]
-console.log ("The array of information leads me to believe that your number is\n:" = travlerinformation);
+var age = 27;
+var height = 6.0;
+var weight = 205;
+var birthLocation = "Chicago";
+var towelCount = 9;
+var travlerinformation = [age, height, weight,birthLocation, towelCount];
+console.log ("The array of information leads me to believe that your number is\n" = travlerinformation);
 console.log (The answer is always 42")
