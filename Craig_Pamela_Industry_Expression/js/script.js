1e06c869700e2a45694cfdd48fdfc8222a8fe1ea
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 01:06:55 -0400

    Industry Expression
    Test checked out. Did calculation and it came out correct

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -1,9 +1,9 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Industry Expression//
-var totalSafeDrops = 1000;
-var beginningCount = 119.19;
-var endingCount = 220.76;
+var totalSafeDrops = 100;
+var beginningCount = 119;
+var endingCount = 205;
 var creditandDebitTotal =504.45;
 var totalmoney = (totalSafeDrops - beginningCount + endingCount) + (creditandDebitTotal);
 console.log (totalmoney)
