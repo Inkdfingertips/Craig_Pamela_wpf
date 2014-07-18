181b6b8ba43119e3348f95e85c541695996b5213
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-17 22:31:35 -0400

    negative second try
    This is my second attempt at the negatives. The decimal points look
    really weird. It’s right but I don’t like the way it looks. wish I
    could make it into a fraction.

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -1,11 +1,11 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Wacky Expression//
-var totalSafedrops = 12;
+var totalSafedrops = -12;
 //How much money did you drop into the safe over the course of the night
 var beginningTotal = 101.11;
 //How much money did you have in your register at the beinging of your shift
-var endingTotal = -12.00
+var endingTotal = 114.00
 //How much money did you leave the next shift in the register
 var EndOfDayTotal = (totalSafedrops - beginningTotal )+ (endingTotal);
 //This is your end of day total
