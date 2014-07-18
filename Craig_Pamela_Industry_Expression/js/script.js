0b0a86a4d9a3f0b9c30d2bc7e02119e5ece89442
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 01:33:30 -0400

    industry Expression
    Added a Foodstamp var to the equation

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -5,6 +5,7 @@ var totalSafeDrops = 750;
 var beginningCount = 101.62;
 var endingCount = 197.80;
 var creditandDebitTotal =143.19;
+var foodstamps= 4.45
 var totalmoney = (totalSafeDrops - beginningCount + endingCount) + (creditandDebitTotal);
 var mopSales = 560;
 var cashOverOrShort =("This is what you are short or over \n" ) +  (totalmoney - mopSales);
