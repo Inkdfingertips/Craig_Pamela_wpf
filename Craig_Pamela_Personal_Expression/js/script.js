87878f01c62067d91a936754f5943e2c6120deac
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 02:16:34 -0400

    Personal Expression
    What about the possibility of someone drinking out of more than one cup
    a day? Added personalCoffeeCup

--- Craig_Pamela_Personal_Expression/js/script.js
+++ Craig_Pamela_Personal_Expression/js/script.js
@@ -1,7 +1,7 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Personal Expression//
-var coffeCup = 3;
+var personalCoffeCup = 3;
 var ouncesOfCoffee=28;
 var days = 6;
 var total = (coffeCup * ouncesOfCoffee * days);
