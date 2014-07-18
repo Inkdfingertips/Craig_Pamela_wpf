6aab22f25d43a78e736ee5ce72bfbb8dd909b1ae
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 01:41:41 -0400

    Industry Expressions
    I thought it was coming out wrong but it was just human error
    . Everything checks out. We are done here.

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -8,7 +8,8 @@ var creditandDebitTotal =143.19;
 var foodstamps= 4.45;
 var coupons = 1.00;
 var lottopayout = 60.00;
-var totalmoney = (totalSafeDrops - beginningCount + endingCount) + (creditandDebitTotal);
+var totalmoney = (totalSafeDrops - beginningCount + endingCount) + 
+(creditandDebitTotal + foodstamps +coupons +lottopayout);
 var mopSales = 560;
 var cashOverOrShort =("This is what you are short or over \n" ) +  (totalmoney - mopSales);
 console.log (cashOverOrShort)
