90bf5df0b557d5db480788d8b71ff9544a32f37f
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 02:22:50 -0400

    personal Expression
    Having the equal signs so close concerns me so i spaced them out

--- Craig_Pamela_Personal_Expression/js/script.js
+++ Craig_Pamela_Personal_Expression/js/script.js
@@ -4,9 +4,9 @@
 var personalCoffeCup = 3;
 var workCoffeeCup = 14
 var toGoCofeeCup = 5
-var ouncesOfCoffeeForpersdonal=28;
-var ouncesOfCoffeeForWork=
-var ouncesOfCoffeeForToGo=
+var ouncesOfCoffeeForpersdonal= 28;
+var ouncesOfCoffeeForWork =
+var ouncesOfCoffeeForToGo =
 var days = 6;
 var total = (coffeCup * ouncesOfCoffee * days);
 console.log ("The total number of ounces you've drank this week is:\n" + total);
