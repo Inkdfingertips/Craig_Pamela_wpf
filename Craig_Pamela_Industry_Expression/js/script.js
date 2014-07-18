dc91fbecd8f1a4649c79de8d18824d8ca1191345
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 00:50:44 -0400

    Industry Expression
    Typed out my first few var. Now to test it out.

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -1,3 +1,8 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Industry Expression//
+var totalSafeDrops = 
+var beginningCount = 
+var endingCount = 
+var creditandDebitTotal =
+var totalmoney = (totalSafeDrops- beginningCount + endingCount) + (creditandDebitTotal);
