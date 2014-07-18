c2bce542f0277e598384876e1482b207f7ee99ac
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 03:47:52 -0400

    Wacky Expression
    Test 2

--- Craig_Pamela_Wacky_Expression/js/script.js
+++ Craig_Pamela_Wacky_Expression/js/script.js
@@ -1,11 +1,11 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Wacky Expression//
-var age = 30;
-var height = 17.0;
-var weight = 2100;
-var birthLocation = "Mars";
-var towelCount = 0;
+var age = 2;
+var height = 13.0;
+var weight = 15;
+var birthLocation = "Nowhere";
+var towelCount = 300;
 var travlerInformation = [age, height, weight,birthLocation, towelCount];
 ("The array of information leads me to believe that your number is:\n" + travlerInformation)
 console.log ("The answer is always 42")
