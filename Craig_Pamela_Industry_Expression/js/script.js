89b56339c02dfb34a1010ba66ccc065898605c16
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 00:56:20 -0400

    Industry Expression no semi colons
    Forgot semi colons. They are added now. Trying test again

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -1,8 +1,9 @@
 //Pamela Craig WPF 
 //WDD144-O sec 01 
 //Industry Expression//
-var totalSafeDrops = 
-var beginningCount = 
-var endingCount = 
-var creditandDebitTotal =
-var totalmoney = (totalSafeDrops- beginningCount + endingCount) + (creditandDebitTotal);
+var totalSafeDrops = 1000;
+var beginningCount = 119.19;
+var endingCount = 220.76;
+var creditandDebitTotal =504.45;
+var totalmoney = (totalSafeDrops - beginningCount + endingCount) + 
+(creditandDebitTotal);
