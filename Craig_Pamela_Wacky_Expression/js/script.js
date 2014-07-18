ade60c12e8770e59dc268dbf3796e831c5c4ce35
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 04:22:04 -0400

    wacky Expression
    had to add comments

--- Craig_Pamela_Wacky_Expression/js/script.js
+++ Craig_Pamela_Wacky_Expression/js/script.js
@@ -2,10 +2,18 @@
 //WDD144-O sec 01 
 //Wacky Expression//
 var age = 21;
+//insert your age//
 var height = 6.0;
+//how tall are you from the planet's surface//
 var weight = 15;
+//how much do you weight, keep in mind the gravity//
 var birthLocation = "Nowhere";
+//where were you created//
 var towelCount = 300;
+//how many towels are in your possession//
 var travlerInformation = [age, height, weight,birthLocation, towelCount];
+//all of the numbers and you location above equals you//
 ("The array of information leads me to believe that your number is:\n" + travlerInformation)
+//None of this really matters because the answer is always//
 console.log ("The answer is always 42")
+//42//
