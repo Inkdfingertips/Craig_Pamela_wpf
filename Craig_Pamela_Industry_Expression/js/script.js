7410a6c0344a16aa4211bbdd426f8b98af12b0ee
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 01:34:43 -0400

    Industry Expression
    Added a coupon Var

--- Craig_Pamela_Industry_Expression/js/script.js
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -6,6 +6,7 @@ var beginningCount = 101.62;
 var endingCount = 197.80;
 var creditandDebitTotal =143.19;
 var foodstamps= 4.45
+var coupons = 1.00
 var totalmoney = (totalSafeDrops - beginningCount + endingCount) + (creditandDebitTotal);
 var mopSales = 560;
 var cashOverOrShort =("This is what you are short or over \n" ) +  (totalmoney - mopSales);
