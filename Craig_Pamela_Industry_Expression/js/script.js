d692434f60d0b42c366347346df80268f9760a33
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-18 01:01:18 -0400

    Industry Expression More Var
    Added more Vars than the original just to see what would happen.

---
+++ Craig_Pamela_Industry_Expression/js/script.js
@@ -0,0 +1,9 @@
+//Pamela Craig WPF 
+//WDD144-O sec 01 
+//Industry Expression//
+var totalSafeDrops = 1000;
+var beginningCount = 119.19;
+var endingCount = 220.76;
+var creditandDebitTotal =504.45;
+var totalmoney = (totalSafeDrops - beginningCount + endingCount) + 
+(creditandDebitTotal)
