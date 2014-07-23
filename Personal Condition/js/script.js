95e140edcab02083e22eb8e164f9c64db6fbf0c9
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-23 15:57:37 -0400

    The first run of Personal Condition
    10 changes commited & synced

---
+++ Personal Condition/js/script.js
@@ -0,0 +1,9 @@
+var paycheck = 550;
+var insuranceStartUp = 176;
+var dreamCarDownpayment = 1000;
+
+if(paycheck > insuranceStartUp + dreamCarDownpayment){
+	console.log ("Go Paint the town red!");
+}else{
+	console.log ("Don't break the bank!");
+}
