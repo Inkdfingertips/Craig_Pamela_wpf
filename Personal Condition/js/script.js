7b811d3d4063663662a975524c64883e2fad21fc
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-23 17:27:50 -0400

    Personal Condition
    added an extra “{“ after else.
    added the “=== true” after borrowedFromMom, and remembered a semi colon

--- Personal Condition/js/script.js
+++ Personal Condition/js/script.js
@@ -3,8 +3,9 @@ var panicAtTheDiscoConcert = 200;
 var fallOutBoyConcert = 335;
 var borrowedFromMom = true;
 
-if (paycheck < fallOutBoyConcert || borrowedFromMom){
-	console.log ("Enjoy Vagas Lights at the Panic Concert")
+if (paycheck < fallOutBoyConcert || borrowedFromMom === true){
+	console.log ("Have fun being in Death Valley");
 } else{
-	console.log ("Have fun being alive in Death Valley")
+
+	console.log ("Enjoy Vagas Lights at the Panic Concert");
 }
