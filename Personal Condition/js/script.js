6646b698780806c9be6bf3121778c26bf9f43f4b
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-23 17:14:06 -0400

    Personal Conditions
    The  “||” and me are having some problems.

--- Personal Condition/js/script.js
+++ Personal Condition/js/script.js
@@ -1,9 +1,9 @@
-var paycheck = 600;
+var paycheck = 10.50;
 var panicAtTheDiscoConcert = 200;
 var fallOutBoyConcert = 335;
 var borrowedFromMom = true;
 
-if (paycheck < fallOutBoyConcert){
+if (paycheck < fallOutBoyConcert || borrowedFromMom){
 	console.log ("Enjoy Vagas Lights at the Panic Concert")
 } else{
 	console.log ("Have fun being alive in Death Valley")
