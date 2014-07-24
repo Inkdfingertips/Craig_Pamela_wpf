895190540428b183b7ee7d27cd1e688d9600e104
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-24 09:23:55 -0400

    Industry Condition
    Added a prompt!

--- Industry Condition /js/script.js
+++ Industry Condition /js/script.js
@@ -1,4 +1,4 @@
-var lotteryNumber = 72; 
+var lotteryNumber = prompt("Please enter a number between 1 and 100"); 
 //this is a fixed lottery lotteryNumber
 todaysNumbers = [Math.floor (Math.random()*100) +1,Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1];
 //This generates a random number, if 72 hits you win.
