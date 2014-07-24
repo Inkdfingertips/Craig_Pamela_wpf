54370db881a42b1d1caa6b615d20d9b347281dfd
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-24 09:20:58 -0400

    Personal Conditions
    Forgot to add a prompt, don’t worry its fixed now

--- Personal Condition/js/script.js
+++ Personal Condition/js/script.js
@@ -1,11 +1,16 @@
-var paycheck = 10.50;
+var paycheck = prompt("How much was your paycheck?");
+//prompts user for paycheck amount
 var panicAtTheDiscoConcert = 200;
+//is a concert and is expensive
 var fallOutBoyConcert = 335;
-var borrowedFromMom = true;
-
-if (paycheck < fallOutBoyConcert || borrowedFromMom){
-	console.log ("Have fun being in Death Valley");
+//is even more expensive
+var borrowedFromMom = 10;
+//desperate times call for desperate measures, right?
+if (paycheck > (fallOutBoyConcert + borrowedFromMom) ){
+	console.log ("Have fun in Death Valley with Fall Out Boy");
+//if the paycheck is more than the FOB concert plus moms money you can go to the more expensive concert
 } else{
 
 	console.log ("Enjoy Vegas Lights at the Panic Concert");
 }
+//If your paycheck is less that the FOB concert plus moms money, than enjoy Brendon Urie, he's just as good
