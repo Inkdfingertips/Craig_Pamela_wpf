d38d7881a1439373213b36311aaa08c0a766d3e3
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-24 08:41:54 -0400

    Industry Condition
    Added a loop counter, a while,
    and a var test

--- Industry Condition /js/script.js
+++ Industry Condition /js/script.js
@@ -3,3 +3,14 @@ var lotteryNumber = 72;
 todaysNumbers = [Math.floor (Math.random()*100) +1,Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1];
 //This generates a random number, if 72 hits you win.
 Console.log ("Your Numbers Are:" + todaysNumbers);
+var loopCounter = 0;
+//this is where the loop starts
+while (loopCounter < 5)
+//we only have four numbers for the customer to play at a time. Ths is less than five
+{var test = 0;
+if (lotteryNumber == todaysNumbers [test]) {
+	console.log ("Early Retirement ahead!")
+}
+else {test++}
+loopCounter++;}
+//This counts up the loop
