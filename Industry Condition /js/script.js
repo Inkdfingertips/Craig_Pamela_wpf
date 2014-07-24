c0b976aaa31f229589f043288a0bd1e6c1b4342d
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-24 08:27:54 -0400

    industry Condition
     Added the console.log and fixed the bract
    problem.

--- Industry Condition /js/script.js
+++ Industry Condition /js/script.js
@@ -1,4 +1,5 @@
 var lotteryNumber = 72; 
 //this is a fixed lottery lotteryNumber
-todaysNumbers = [Math.floor (Math.random()*100) +1,Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1]]
+todaysNumbers = [Math.floor (Math.random()*100) +1,Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1, Math.floor (Math.random()*100) +1];
 //This generates a random number, if 72 hits you win.
+Console.log ("Your Numbers Are:" + todaysNumbers);
