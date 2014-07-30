960733fca44e875b9c13084e321dc58cf06d81a7
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-30 10:16:21 -0400

    Wacky Function Variables
    Placed in all the variables

--- Function_Wacky/js/script.js
+++ Function_Wacky/js/script.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+String sentence = prompt("Please type a funny sentence.")
+int length1= sentence.length();
+int digit = 1;
+char character = sentence.charAt(digit);
+
