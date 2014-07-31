c027568109ad281edb363143580a448d89b73f90
Inkdfingertips <Alixandraway@fullsail.edu>
2014-07-31 12:35:44 -0400

    WackyFunctions
    Took the skelton of the last product and gave it a function

--- Function_Wacky/js/script.js
+++ Function_Wacky/js/script.js
@@ -1,25 +1,14 @@
 //Pamela Craig
 //7/30/14
 //Wacky Function  
-var partyCannon = prompt("How many guests (in tens between 10 and 50) will be at your party?"); 
+var partyCannon = prompt("How many guests (at least ten) will be at your party?"); 
 
-while (partyCannon == " ") {
+//while (partyCannon == " ") {
 	//re-prompt user
-	console.log ("How many guests (in tens between 10 and 50) will be at your party?");
-}
+	//console.log ("How many guests (at least ten) will be at your party?");
 
-if (partyCannon === 10)
-	{console.log("Please stuff 10 cupcakes, 2 cakes, seven streamers and 3 tables into the party cannon. Blast off for a private party!"); 
-}
- if (partyCannon === 20)
-	{console.log ("Please stuff 20 cupcakes, 3 cakes, 10 streamers, 6 tables and a karaoke machine into the party cannon! Rock it out with your buds!"); 
-}
- if (partyCannon === 30) 
-	{console.log ("Please stuff 30 cupcakes, 4 cakes, 20 streamers, 8 tables, and a DJ into the party cannon! Have fun with the pony-folk!");
-}
- if (partyCannon === 40) 
-	{console.log ("Please stuff 40 cupcakes, 6 cakes, 30 streamers, 10 tables, 2 DJs, and the minibar into the party cannon! Have fun with the party!");
-}
- if (partyCannon === 50) 
-	{console.log ("Please stuff 50 cupcakes, 8 cakes, 46 streamers, 12 tables, the DJ, dance floor, and one full bar into the party cannon! Watch your drink, though....");
-}
+	function calculatePartyCannon ( partyCannon )
+	{
+		var stuff = ("Please stuff " + partyCannon +"cupcakes" + partyCannon/5 + "cakes" + partyCannon/2 +"streamers" + partyCannon/5 + "tables into the party cannon. Have a good time!");
+		return stuff;
+	}
